b5185bcc78c5a8fcaf680f7c93ad5451344db26e
Mirko Schaefer <codewithmirko@gmail.com>
2024-05-28 11:37:49 +0200

    Solved lab again to practice after bootcamp

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = s1.concat(" ", s2, " ", s3, " ", s4, " ", s5);
 
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -21,26 +20,31 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
+console.log(part1.length);
+
+const CamelTrail =
+  part1.slice(0, part1.length - 1) +
+  part1[part1.length - 1].toUpperCase() +
+  part2.slice(0, part2.length - 1) +
+  part2[part2.length - 1].toUpperCase();
+
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(CamelTrail);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
+const tipPercentage = 15;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = (billTotal * tipPercentage) / 100;
 
 // Print out the tipAmount
-
-
-
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,10 +52,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomNum = Math.ceil(Math.random() * 10);
 
 // Print the generated random number
 
-
+console.log(randomNum);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +67,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+//false
 
 const expression2 = a || b;
+//true
 
 const expression3 = !a && b;
+// false
 
 const expression4 = !(a && b);
+// true
 
 const expression5 = !a || !b;
+// true
 
 const expression6 = !(a || b);
+// false
 
 const expression7 = a && a;
+// true
